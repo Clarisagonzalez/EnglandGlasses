4a6240c0a645ca7c02d9107f63f50b03df07945a
Harriet Rowan <harrietrowan01@gmail.com>
2024-12-10 15:21:49 -0500

    Started Fixing the Frames Page

--- js/frames.js
+++ js/frames.js
@@ -1,248 +1,219 @@
-const frames = [
-  {
-    title: 'Stylish Frame 1',
-    price: '$120',
-    colors: [
-      { color: 'Black', src: 'images/glasses/A5.0/A5.0 Black.jpg' },
-      { color: 'Burnt Orange', src: 'images/glasses/A5.0/A5.0 Burnt Orange.jpg' }
-    ],
-    link: 'frame-details.html?frame=1'
-  },
-  {
-    title: 'Stylish Frame 2',
-    price: '$130',
-    colors: [
-      { color: 'Black', src: 'images/glasses/A5.0/A5.1 Black.jpg' },
-      { color: 'Brown', src: 'images/glasses/A5.0/A5.1 Tortoise.jpg' }
-    ],
-    link: 'frame-details.html?frame=2'
-  },
-  {
-    title: 'Stylish Frame 3',
-    price: '$140',
-    colors: [
-      { color: 'Black', src: 'images/glasses/A5.0/A5.2 Black & Red.jpg' },
-      { color: 'Gray', src: 'images/glasses/A5.0/.jpg' }
-    ],
-    link: 'frame-details.html?frame=3'
-  },
-  {
-    title: 'Stylish Frame 4',
-    price: '$150',
-    colors: [
-      { color: 'Black', src: 'images/glasses/A5.0/A5.3 Black.jpg' },
-      { color: 'Red', src: 'images/glasses/A5.0/A5.3 Blue.jpg' }
-    ],
-    link: 'frame-details.html?frame=4'
-  },
-  {
-    title: 'Stylish Frame 5',
-    price: '$160',
-    colors: [
-      { color: 'Black', src: 'images/glasses/A5.0/A5.4 Black.jpg' },
-      { color: 'Silver', src: 'images/glasses/A5.0/A5.4 Pink.jpg' }
-    ],
-    link: 'frame-details.html?frame=5'
-  },
-  {
-    title: 'Stylish Frame 6',
-    price: '$170',
-    colors: [
-      { color: 'Black', src: 'images/glasses/A5.0/A5.5 Black & Clear Gradient.jpg' },
-      { color: 'Gold', src: 'images/glasses/A5.0/A5.5 Blue & Clear Gradient.jpg' },
-      { color: 'Gold', src: 'images/glasses/A5.0/A5.5 Pink & Clear Gradient.jpg' }
-    ],
-    link: 'frame-details.html?frame=6'
-  },
-  {
-    title: 'Stylish Frame 7',
-    price: '$180',
-    colors: [
-      { color: 'Black', src: 'images/glasses/A5.0/A5.6 Black & Clear.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.6 Blue & Clear.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.6 Pink & Clear.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.6 Tortoise.jpg' }
-      
-    ],
-    link: 'frame-details.html?frame=7'
-  },
-  {
-    title: 'Stylish Frame 8',
-    price: '$190',
-    colors: [
-      { color: 'Black', src: 'images/glasses/A5.0/A5.8 Black & Blue.jpg' },
-      { color: 'Pink', src: 'images/glasses/A5.0/A5.8 Black.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.8 Clear.jpg' }
+document.addEventListener('DOMContentLoaded', function () {
+  // Define categories and subcategories
+  const categories = [
+    {
+      category: 'A5.0',
+      subcategories: [
+        {
+          title: 'A5.0',
+          price: '$120',
+          colors: [
+            { color: 'Black', src: 'images/glasses/A5.0/A5.0 Black.jpg' },
+            { color: 'Burnt Orange', src: 'images/glasses/A5.0/A5.0 Burnt Orange.jpg' },
+          ],
+        },
+        {
+          title: 'A5.1',
+          price: '$130',
+          colors: [
+            { color: 'Black', src: 'images/glasses/A5.0/A5.1 Black.jpg' },
+            { color: 'Brown', src: 'images/glasses/A5.0/A5.1 Tortoise.jpg' },
+          ],
+        },
+        {
+          title: 'A5.2',
+          price: '$140',
+          colors: [
+            { color: 'Black & Red', src: 'images/glasses/A5.0/A5.2 Black & Red.jpg' },
+          ],
+        },
+        {
+          title: 'A5.3',
+          price: '$130',
+          colors: [
+            {color: 'Black', src: 'images/glasses/A5.0/A5.3 Black.jpg'},
+            {color: 'Blue', src: 'images/glasses/A5.0/A5.3 Blue.jpg'},
+          ],
+        },
+        {
+          title: 'A5.4',
+          price: '$120',
+          colors: [
+            {color: 'Black', src: 'images/glasses/A5.0/A5.4 Black.jpg'},
+            {color: 'Pink', src: 'images/glasses/A5.0/A5.4 Pink.jpg'},
+          ],
+        },
+        {
+          title: 'A5.5',
+          price: '$140',
+          colors: [
+            {color: 'Black & Clear', src: 'images/glasses/A5.0/A5.5 Black & Clear Gradient.jpg'},
+            {color: 'Blue & Clear', src: 'images/glasses/A5.0/A5.5 Blue & Clear Gradient.jpg'},
+            {color: 'Pink & Clear', src: 'images/glasses/A5.0/A5.5 Pink & Clear Gradient.jpg'},
+          ],
+        },
+        {
+          title: 'A5.6',
+          price: '$130',
+          colors: [
+            {color: 'Black & Clear', src: 'images/glasses/A5.0/A5.6 Black & Clear.jpg'},
+            {color: 'Blue & Clear', src: 'images/glasses/A5.0/A5.6 Blue & Clear.jpg'},
+            {color: 'Pink & Clear', src: 'images/glasses/A5.0/A5.6 Pink & Clear.jpg'},
+            {color: 'Tortoise', src: 'images/glasses/A5.0/A5.6 Tortoise.jpg'},
+          ],
+        },
+        {
+          title: 'A5.8',
+          price: '$140',
+          colors: [
+            {color: 'Black & Blue', src: 'images/glasses/A5.0/A5.8 Black & Blue.jpg'},
+            {color: 'Black', src: 'images/glasses/A5.0/A5.8 Black.jpg'},
+            {color: 'Clear', src: 'images/glasses/A5.0/A5.8 Clear.jpg'},
+          ],
+        },
+        {
+          title: 'A5.9',
+          price: '$120',
+          colors: [
+            {color: 'Black', src: 'images/glasses/A5.0/A5.9 Black.jpg'},
+            {color: 'Blue', src: 'images/glasses/A5.0/A5.9 Blue.jpg'},
+            {color: 'Clear', src: 'images/glasses/A5.0/A5.9 Clear.jpg'},
+          ],
+        },
+        {
+          title: 'A5.91',
+          price: '$130',
+          colors: [
+            {color: 'Black', src: 'images/glasses/A5.0/A5.91 Black.jpg'},
+            {color: 'Blue', src: 'images/glasses/A5.0/A5.91 Blue.jpg'},
+            {color: 'Brown', src: 'images/glasses/A5.0/A5.91 Brown.jpg'},
+          ],
+        },
+        {
+          title: 'A5.92',
+          price: '$130',
+          colors: [
+            {color: 'Black & Brown', src: 'images/glasses/A5.0/A5.92 Black & Brown.jpg'},
+          ],
+        },
+        {
+          title: 'A5.93',
+          price: '$130',
+          colors: [
+            {color: 'Black & Clear', src: 'images/glasses/A5.0/A5.93 Black & Clear.jpg'},
+            {color: 'Black & Pink', src: 'images/glasses/A5.0/A5.93 Black & Pink.jpg'},
+            {color: 'Brown & Pink', src: 'images/glasses/A5.0/A5.93 Brown & Pink.jpg'},
+          ],
+        },
+        {
+          title: 'A5.94',
+          price: '$130',
+          colors: [
+            {color: 'Black & Brown', src: 'images/glasses/A5.0/A5.94 Black & Brown.jpg'},
+            {color: 'Clear & Silver', src: 'images/glasses/A5.0/A5.94 Clear & Silver.jpg'},
+            {color: 'Clear', src: 'images/glasses/A5.0/A5.94 Clear.jpg'},
+          ],
+        },
+        {
+          title: 'A5.95',
+          price: '$130',
+          colors: [
+            {color: 'Black & Silver', src: 'images/glasses/A5.0/A5.95 Black & Silver.jpg'},
+            {color: 'Black', src: 'images/glasses/A5.0/A5.95 Black.jpg'},
+            {color: 'Clear', src: 'images/glasses/A5.0/A5.95 Clear.jpg'},
+          ],
+        },
+      ],
+    },
+    {
+      category: 'Academic',
+      subcategories: [
+        {
+          title: 'P2027',
+          price: '$130',
+          colors: [
+            {color: 'Black', src: 'images/glasses/Academic/P2027 Black.jpg'},
+            {color: 'Red', src: 'images/glasses/Academic/P2027 Red.jpg'},
+          ],
+        },
+        {
+          title: 'P2031',
+          price: '$120',
+          colors: [
+            {color: 'Black', src: 'images/glasses/Academic/P2031 Black.jpg'},
+            {color: 'Grey', src: 'images/glasses/Academic/P2031 Grey.jpg'},
+          ],
+        },
+      ],
+    },
+  ];
 
-    ],
+  // Select container for frames
+  const framesContainer = document.querySelector('.frames-container');
 
-    link: 'frame-details.html?frame=8'
-  },
-  {
-    title: 'Stylish Frame 9',
-    price: '$200',
-    colors: [
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.9 Black.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.9 Blue.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.9 Clear.jpg' }
-    ],
-    link: 'frame-details.html?frame=9'
-  },
-  {
-    title: 'Stylish Frame 10',
-    price: '$210',
-    colors: [
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.91 Black.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.91 Blue.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.91 Brown.jpg' }
-    ],
-    link: 'frame-details.html?frame=10'
-  },
-  {
-    title: 'Stylish Frame 11',
-    price: '$220',
-    colors: [
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.92 Black & Brown.jpg' },
-      
-    ],
-    link: 'frame-details.html?frame=11'
-  },
-  {
-    title: 'Stylish Frame 12',
-    price: '$230',
-    colors: [
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.93 Black & Clear.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.93 Black & Pink.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.93 Brown & Pink.jpg' }
-    ],
-    link: 'frame-details.html?frame=12'
-  },
-  {
-    title: 'Stylish Frame 13',
-    price: '$240',
-    colors: [
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.94 Black & Brown.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.94 Clear & Silver.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.94 Clear.jpg' }
-    ],
-    link: 'frame-details.html?frame=13'
-  },
-  {
-    title: 'Stylish Frame 14',
-    price: '$250',
-    colors: [
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.95 Black & Silver.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.95 Black.jpg' },
-      { color: 'Blue', src: 'images/glasses/A5.0/A5.95 Clear.jpg' }
-    ],
-    link: 'frame-details.html?frame=14'
-  },
-  {
-    title: 'Stylish Frame 15',
-    price: '$260',
-    colors: [
-      { color: 'Black', src: 'images/glasses/Academic/P2027 Black.jpg' },
-      { color: 'Silver', src: 'images/frames/frame15_silver.jpg' }
-    ],
-    link: 'frame-details.html?frame=15'
-  },
-  {
-    title: 'Stylish Frame 16',
-    price: '$270',
-    colors: [
-      { color: 'Black', src: 'images/glasses/Academic/P2027 Red.jpg' },
-      { color: 'Gold', src: 'images/frames/frame16_gold.jpg' }
-    ],
-    link: 'frame-details.html?frame=16'
-  },
-  {
-    title: 'Stylish Frame 17',
-    price: '$280',
-    colors: [
-      { color: 'Black', src: 'images/glasses/Academic/P2031 Black.jpg' },
-      { color: 'Blue', src: 'images/frames/frame17_blue.jpg' }
-    ],
-    link: 'frame-details.html?frame=17'
-  },
-  {
-    title: 'Stylish Frame 18',
-    price: '$290',
-    colors: [
-      { color: 'Black', src: 'images/glasses/Academic/P2031 Grey.jpg' },
-      { color: 'Purple', src: 'images/frames/frame18_purple.jpg' }
-    ],
-    link: 'frame-details.html?frame=18'
-  },
-  {
-    title: 'Stylish Frame 19',
-    price: '$300',
-    colors: [
-      { color: 'Black', src: 'images/frames/frame19_black.jpg' },
-      { color: 'Green', src: 'images/frames/frame19_green.jpg' }
-    ],
-    link: 'frame-details.html?frame=19'
-  },
-  {
-    title: 'Stylish Frame 20',
-    price: '$230',
-    colors: [
-      { color: 'Black', src: 'images/frames/frame20_black.jpg' },
-      { color: 'Pink', src: 'images/frames/frame20_pink.jpg' }
-    ],
-    link: 'frame-details.html?frame=20'
-  }
-];
-
-// Select the container where the frames will be inserted
-const framesContainer = document.querySelector('.frames-container');
-
-// Loop through the array of frames and generate the HTML for each
-frames.forEach(frame => {
-  // Generate the HTML for the color options (radio buttons)
-  const colorOptionsHTML = frame.colors.map(color => {
-    return `
-      <input type="radio" id="${frame.title}-${color.color}" name="color-${frame.title}" value="${color.color}" ${color.color === 'Black' ? 'checked' : ''}>
-      <label for="${frame.title}-${color.color}">${color.color}</label>
-    `;
-  }).join('');
-
-  // Generate the HTML for the frame (with dynamic color options)
-  const frameHTML = `
-    <div class="col-md-4 mb-4">
-      <div class="card">
-        <!-- Image that will update dynamically -->
-        <img id="frame-image-${frame.title}" src="${frame.colors[0].src}" class="card-img-top" alt="${frame.title}">
-        <div class="card-body">
-          <h5 class="card-title">${frame.title}</h5>
-          <p class="card-text">${frame.price}</p>
-
-          <!-- Color selection options -->
-          <div>
-            <label>Choose Color:</label><br>
-            ${colorOptionsHTML}
-          </div>
-
-          <a href="${frame.link}" class="btn btn-primary">Select</a>
+  // Generate HTML for each category and its subcategories
+  categories.forEach((category, categoryIndex) => {
+    // Generate category button and container for subcategories
+    const categoryHTML = `
+      <div class="category mb-4">
+        <button class="btn btn-secondary w-100 text-left category-btn" data-category="${categoryIndex}">
+          ${category.category}
+        </button>
+        <div class="subcategories collapse mt-3" id="category-${categoryIndex}">
+          <!-- Subcategories will be injected here -->
         </div>
       </div>
-    </div>
-  `;
+    `;
+    framesContainer.insertAdjacentHTML('beforeend', categoryHTML);
 
-  // Insert the generated HTML into the container
-  framesContainer.innerHTML += frameHTML;
-});
+    // Populate subcategories for this category
+    const subcategoriesContainer = document.querySelector(`#category-${categoryIndex}`);
+    category.subcategories.forEach((subcat, subcatIndex) => {
+      const colorOptionsHTML = subcat.colors
+        .map(
+          (color, colorIndex) => `
+          <input type="radio" id="color-${categoryIndex}-${subcatIndex}-${colorIndex}" name="color-${categoryIndex}-${subcatIndex}" value="${color.color}" ${colorIndex === 0 ? 'checked' : ''}>
+          <label for="color-${categoryIndex}-${subcatIndex}-${colorIndex}">${color.color}</label>
+        `
+        )
+        .join('');
 
-// Add event listeners for color change
-frames.forEach(frame => {
-  frame.colors.forEach(color => {
-    const colorRadio = document.querySelector(`#${frame.title}-${color.color}`);
-    const imageElement = document.getElementById(`frame-image-${frame.title}`);
-    
-    // When a color is selected, update the image source
-    colorRadio.addEventListener('change', function() {
-      if (this.checked) {
-        imageElement.src = color.src; // Update the image based on selected color
-      }
+      const subcategoryHTML = `
+        <div class="card">
+          <img id="frame-image-${categoryIndex}-${subcatIndex}" src="${subcat.colors[0].src}" class="card-img-top" alt="${subcat.title}">
+          <div class="card-body">
+            <h5 class="card-title">${subcat.title}</h5>
+            <p class="card-text">${subcat.price}</p>
+            <div>
+              <label>Choose Color:</label><br>
+              ${colorOptionsHTML}
+            </div>
+            <a href="frame-details.html?category=${categoryIndex}&subcategory=${subcatIndex}" class="btn btn-primary mt-2">View Details</a>
+          </div>
+        </div>
+      `;
+      subcategoriesContainer.insertAdjacentHTML('beforeend', subcategoryHTML);
+
+      // Add event listeners for color selection
+      subcat.colors.forEach((color, colorIndex) => {
+        const colorRadio = document.querySelector(`#color-${categoryIndex}-${subcatIndex}-${colorIndex}`);
+        const imageElement = document.getElementById(`frame-image-${categoryIndex}-${subcatIndex}`);
+
+        colorRadio.addEventListener('change', function () {
+          if (this.checked) {
+            imageElement.src = color.src;
+          }
+        });
+      });
     });
+  });
+
+  // Add event listeners to toggle subcategories
+  framesContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('category-btn')) {
+      const categoryIndex = event.target.dataset.category;
+      const subcategories = document.querySelector(`#category-${categoryIndex}`);
+      subcategories.classList.toggle('collapse');
+    }
   });
 });
